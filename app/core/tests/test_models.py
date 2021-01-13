d2a3dc0cfaeb63ddf503395b22eda18a4250aa99
JonathanOzsvath <ozsvathjonathan@gmail.com>
2021-01-13 11:11:15 +0100

    Added custem user model.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@vmi.com'
+        passord = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=passord
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(passord))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "test@ALSDK.COM"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='test123'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'admin@admin.com',
+            'admin123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
